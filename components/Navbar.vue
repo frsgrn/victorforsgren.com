394806dbc98be7b90a16d34ca76570d31b9e44a7
Victor Forsgren <forsgren.victor@gmail.com>
2018-12-02 20:10:47 +0100

    Väldigt renligt!

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -18,13 +18,15 @@ li {
   margin: 9px;
   text-decoration: none;
   padding: 3px;
-  font-size: 20px;
+  font-size: 21px;
   border-color: rgba(0,0,0,0);
   color: rgb(50, 54, 72);
   font-family: "Tinos", serif;
   transition: border-color 0.3s ease-in-out;
 }
-
+.navbar .nuxt-link-exact-active {
+  border-bottom: 2px solid #3498db;
+}
 .navbar li a:hover {
   border-bottom: 2px solid #3498db;
   margin-bottom: -1px;
