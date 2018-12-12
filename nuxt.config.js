43b55163d9ad708a569eb1f37bee4b22278ae3bf
Victor Forsgren <forsgren.victor@gmail.com>
2018-12-12 16:39:08 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Victor Forsgren' }
+      { hid: 'description', name: 'description', content: 'Victor Forsgrens sida på internet.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
