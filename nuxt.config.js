ce1090810177bf59ea0d4c41a86f2f02a14935a5
Victor Forsgren <forsgren.victor@gmail.com>
2019-01-02 21:50:32 +0100

    nu jävlar säger leffe

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,7 @@ module.exports = {
   /*
   ** Headers of the page
   */
-  modules: ['@nuxtjs/dotenv', '@nuxtjs/google-analytics'],
-  'google-analytics': {
-    id: process.env.ANALYTICS_ID
-  },
+  modules: ['@nuxtjs/dotenv', ['@nuxtjs/google-analytics', { id: process.env.ANALYTICS_ID }]],
   head: {
     title: 'Victor Forsgren',
     meta: [
